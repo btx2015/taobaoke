a48ea733a29e6a780bd6eb198661f6e911765d36
btx2015 <1157621577@qq.com>
2019-03-13 21:47:58 +0800

    增加当前菜单展示逻辑

--- Application/Admin/Controller/CommonController.class.php
+++ Application/Admin/Controller/CommonController.class.php
@@ -16,27 +16,46 @@ class CommonController extends Controller
 
     public function _initialize()
     {
-//        if(empty($_SESSION['adminInfo']['id']))
-//            $this->redirect('System/Login/login');
-//
-//        $lastLoginTime = strtotime($_SESSION['adminInfo']['last_login_time']);
-//        if(time() - $lastLoginTime > 3600)
-//            $this->redirect('System/Login/login');
-//
-//        if(!checkAccess()){
-//            if(IS_POST)
-//                showError(10010);
-//            else{
-//                unset($_SESSION['adminInfo']);
-//                $this->redirect('System/Login/login');
-//            }
-//        }
-//
-//        if(IS_GET){
-//            $this->assign('menuData',S('role_menu_'.$_SESSION['adminInfo']['role_id']));
-            $this->assign('menuData',S('role_menu_1'));
-            $this->assign('active',strtolower(CONTROLLER_NAME.'/'.ACTION_NAME));
-//        }
+        if(empty($_SESSION['adminInfo']['id']))
+            $this->redirect('System/Login/login');
+
+        $lastLoginTime = strtotime($_SESSION['adminInfo']['last_login_time']);
+        if(time() - $lastLoginTime > 3600)
+            $this->redirect('System/Login/login');
+
+        if(!checkAccess()){
+            if(IS_POST)
+                showError(10010);
+            else{
+                unset($_SESSION['adminInfo']);
+                $this->redirect('System/Login/login');
+            }
+        }
+
+        $this->getMenuData();
     }
 
+    private function getMenuData(){
+        if(IS_GET){
+            $menuData = S('role_menu_1');
+            if ($menuData) {
+                $active = strtolower(CONTROLLER_NAME . '/' . ACTION_NAME);
+                foreach ($menuData as &$v) {
+                    if (isset($v['children'])) {
+                        foreach ($v['children'] as &$a) {
+                            if ($a['path'] === $active) {
+                                $a['active'] = 1;
+                                $v['active'] = 1;
+                                break;
+                            }
+                        }
+                        if (isset($v['active'])) {
+                            break;
+                        }
+                    }
+                }
+            }
+            $this->assign('menuData', $menuData);
+        }
+    }
 }
